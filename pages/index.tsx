e5cd65f667906fd835b0b94c08d60fcf07bd1fc2
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-25 15:13:46 -0300

    tarjetas en el index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,32 @@
 import type { NextPage } from "next";
-import { Typography } from "@mui/material";
+import { Card, CardContent, CardHeader, Grid } from "@mui/material";
 import { Layout } from "../components/layouts";
 
 const HomePAge: NextPage = () => {
    return (
-      <Layout>
-         <Typography variant="h1" color="primary">
-            Hola mundo
-         </Typography>
+      <Layout title="Home - OpenJira">
+         <Grid container spacing={2}>
+            <Grid item xs={12} sm={4}>
+               <Card sx={{ height: "calc(100vh - 100px)" }}>
+                  <CardHeader title="Pendientes">
+                     <CardContent>
+                        {/* Agregar una nueva entrada */}
+                        {/* Listado de las entradas */}
+                     </CardContent>
+                  </CardHeader>
+               </Card>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+               <Card sx={{ height: "calc(100vh - 100px)" }}>
+                  <CardHeader title="En Progreso"></CardHeader>
+               </Card>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+               <Card sx={{ height: "calc(100vh - 100px)" }}>
+                  <CardHeader title="Completadas"></CardHeader>
+               </Card>
+            </Grid>
+         </Grid>
       </Layout>
    );
 };
