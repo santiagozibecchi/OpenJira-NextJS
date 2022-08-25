57ec033bb9ce797bd2e6eb19d63fc7b2f6cacc0a
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-25 15:43:37 -0300

    estado inicial - Entries

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,13 +1,36 @@
 import { FC, PropsWithChildren, useReducer } from "react";
+import { v4 as uuidv4 } from "uuid";
 import { Entry } from "../../interfaces";
 import { EntriesContext, entriesReducer } from "./";
+
 // InitialState
 export interface EntriesState {
    entries: Entry[];
 }
 
 const Entries_INITIAL_STATE: EntriesState = {
-   entries: [],
+   entries: [
+      {
+         _id: uuidv4(),
+         description: "Ipsum laborum nulla ex laborum.",
+         status: "pending",
+         createdAt: Date.now(),
+      },
+      {
+         _id: uuidv4(),
+         description:
+            "Eiusmod enim aliqua ad consectetur qui exercitation adipisicing laborum nisi excepteur et.",
+         status: "in-progress",
+         createdAt: Date.now() - 1000000,
+      },
+      {
+         _id: uuidv4(),
+         description:
+            "Sunt sunt non fugiat est velit sunt nulla eu cillum in exercitation elit eu.",
+         status: "finished",
+         createdAt: Date.now() - 100000,
+      },
+   ],
 };
 
 export const EntriesProvider: FC<PropsWithChildren> = ({ children }) => {
