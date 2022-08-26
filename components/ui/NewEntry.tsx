b281179a7ac1efa4bb5738cbdfdeda95a249da44
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-25 21:47:18 -0300

    adding form

--- components/ui/NewEntry.tsx
+++ components/ui/NewEntry.tsx
@@ -1,36 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import SaveIcon from "@mui/icons-material/Save";
 import { Button, TextField } from "@mui/material";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import { Box } from "@mui/system";
 
 export const NewEntry = () => {
+   const [isAdding, setIsAdding] = useState(false);
+
    return (
-      <Box sx={{ marginBotton: 2, paddingX: 1 }}>
-         <Button
-            startIcon={<AddCircleOutlineIcon />}
-            fullWidth
-            variant="outlined"
-         >
-            Agregar tarea
-         </Button>
+      <Box sx={{ marginBottom: 1, paddingX: 1 }}>
+         {isAdding ? (
+            <>
+               <TextField
+                  fullWidth
+                  sx={{ marginTop: 2, marginBottom: 1 }}
+                  placeholder="nueva entrada"
+                  autoFocus
+                  multiline
+                  label="Nueva entrada"
+                  helperText="Ingrese un valor"
+               />
 
-         <TextField
-            fullWidth
-            sx={{ marginTop: 2, marginBottom: 1 }}
-            placeholder="nueva entrada"
-            autoFocus
-            multiline
-            label="Nueva entrada"
-            helperText="Ingrese un valor"
-         />
-
-         <Box display="flex" justifyContent="space-between">
-            <Button variant="text">Cancelar</Button>
-            <Button variant="outlined" color="secondary" endIcon={<SaveIcon />}>
-               Guardar
+               <Box display="flex" justifyContent="space-between">
+                  <Button variant="text" onClick={() => setIsAdding(false)}>
+                     Cancelar
+                  </Button>
+                  <Button
+                     variant="outlined"
+                     color="secondary"
+                     endIcon={<SaveIcon />}
+                  >
+                     Guardar
+                  </Button>
+               </Box>
+            </>
+         ) : (
+            <Button
+               startIcon={<AddCircleOutlineIcon />}
+               fullWidth
+               variant="outlined"
+               onClick={() => setIsAdding(true)}
+            >
+               Agregar tarea
             </Button>
-         </Box>
+         )}
       </Box>
    );
 };
