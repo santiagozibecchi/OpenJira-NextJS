8df08d695dd5bfb0f62a5ac48820f9d22967883a
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-27 16:23:41 -0300

    mongoose - coneccion db estructura basica

---
+++ database/db.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+/**
+ * 0 = disconnected
+ * 1 = connected
+ * 2 = connecting
+ * 3 = disconnecting
+ */
+
+const mongooConecction = {
+   isConnected: 0,
+};
+
+export const connect = async () => {
+   if (mongooConecction.isConnected) {
+      console.log("Ya estamos conectados");
+      return;
+   }
+   // si no estamos conectados reviso las conecctiones en mongoose
+   // Si es mayor a cero significa que hay alguna coneccion adicional
+   // por lo tanto quiero revisar y usar esa coneccion
+   if (mongoose.connections.length > 0) {
+      // Para el estado de la coneccion
+      mongooConecction.isConnected = mongoose.connections[0].readyState;
+      if (mongooConecction.isConnected === 1) {
+         console.log("Usando coneccion anterior");
+         return;
+      }
+      //   Evito tener coneccioness simultaneas
+      await mongoose.disconnect();
+   }
+   await mongoose.connect("...");
+   mongooConecction.isConnected = 1;
+   console.log("Conectado a MongoDB", "....");
+};
+
+export const disconnect = async () => {
+   if (mongooConecction.isConnected !== 0) return;
+
+   await mongoose.disconnect();
+   console.log("Desconectado de MongoDB");
+};
