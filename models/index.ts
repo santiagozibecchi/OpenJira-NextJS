e7eb529f027e2b046ec214e1fb3a260f6ac2fa4a
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-27 18:32:51 -0300

    mostrar entries en postman como respuesta

--- models/index.ts
+++ models/index.ts
@@ -1,1 +1,3 @@
-export { default as EntryModel } from "./Entry";
+export { default as EntryModel, type IEntry } from "./Entry";
+// Se coloca type para decirle que se trata de un tipo de data
+// Porque sino es como que estaria exportando algo que esta vacio
--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -1,11 +1,32 @@
 // RestFull API
 
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { db } from "../../../database";
+import { IEntry, EntryModel } from "../../../models";
 
-type Data = {
-    message: string
-}
+// Forma en la que luce la respuesta
+type Data = { message: string } | IEntry[] | IEntry;
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-    res.status(200).json({ message: 'Example' })
+export default function handler(
+   req: NextApiRequest,
+   res: NextApiResponse<Data>
+) {
+   // Endpoint para crear
+   switch (req.method) {
+      case "GET":
+         return getEntries(res);
+
+      default:
+         return res.status(400).json({ message: "Endpoint no existe" });
+   }
 }
+const getEntries = async (res: NextApiResponse<Data>) => {
+   await db.connect();
+
+   // Obtenemos todas las entradas
+   const entries = await EntryModel.find().sort({ createdAt: "ascending" });
+
+   await db.disconnect();
+
+   res.status(200).json(entries);
+};
