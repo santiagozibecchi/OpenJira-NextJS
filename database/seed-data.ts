efaa00431738126df117242f83d76bdfe8e7a777
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-27 17:24:34 -0300

    borrando los _id de seed-data / id de mongo use

--- database/seed-data.ts
+++ database/seed-data.ts
@@ -4,20 +4,17 @@
 export const seedData = {
    entries: [
       {
-         _id: uuidv4(),
          description: "PENDIENTE: Ipsum laborum nulla ex laborum.",
          status: "pending",
          createdAt: Date.now(),
       },
       {
-         _id: uuidv4(),
          description:
             "EN PROGRESO: Eiusmod enim aliqua ad consectetur qui exercitation adipisicing laborum nisi excepteur et.",
          status: "in-progress",
          createdAt: Date.now() - 1000000,
       },
       {
-         _id: uuidv4(),
          description:
             "TERMINADAS: Sunt sunt non fugiat est velit sunt nulla eu cillum in exercitation elit eu.",
          status: "finished",
