9355b672a77b9bf700c794d81565ad5f22fc4bd5
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-28 18:18:28 -0300

    middleware + bad-request

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,21 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(req: NextRequest) {
-   console.log({ req: req.nextUrl });
+   if (req.nextUrl.pathname.startsWith("/api/entries/")) {
+      const id = req.nextUrl.pathname.replace("/api/entries/", "");
+      const checkMongoIDRegExp = new RegExp("^[0-9a-fA-F]{24}$");
+
+      if (!checkMongoIDRegExp.test(id)) {
+         const url = req.nextUrl.clone();
+         //  redireccion al mensaje de error
+         url.pathname = "/api/bad-request";
+         //  ? => para empezar con los query parameters
+         url.search = `?message${id} is not a valid mongoID`;
+         return NextResponse.rewrite(url);
+      }
+   }
+
+   //console.log({ req: req.nextUrl.pathname });
 
    return NextResponse.next();
 }
@@ -17,3 +31,6 @@ export const config = {
       "/api/entries/:path*",
    ],
 };
+
+// * El middleware solo va a entrar a las peticiones que entren por el
+// * matcher definido
